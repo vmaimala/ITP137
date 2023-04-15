efab3b2a5adaa4c9735a996f9def6e985cd77732
vmaimala1 <125136666+vmaimala@users.noreply.github.com>
2023-04-15 05:01:01 +0000

    Added lists

--- Assignment/Palindrome.swift
+++ Assignment/Palindrome.swift
@@ -1,2 +1,25 @@
 // Palindrome
 
+//List1
+var text = ["1", "0", "8", "0", "1"]
+//List2
+//var text = ["m", "a", "d", "a", "m"]
+//List3
+//var text = ["c", "o", "d", "e"]
+
+//Array
+var reversed = [String]()
+var check = text.count - 1
+
+while check >= 0 {
+  reversed.append(text[check])
+  check -= 1
+}
+
+//Output
+if text == reversed {
+  print("\(text) is a palindrome.")
+}
+else {
+  print("\(text) is not a palindrome.")
+}
